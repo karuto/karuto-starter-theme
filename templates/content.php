d3887226384804642ea33b061117879b220ffedc
Karuto <iamkaruto@gmail.com>
2016-01-24 00:15:41 -0800

    Fixed postid

--- templates/content.php
+++ templates/content.php
@@ -1,7 +1,7 @@
 <article <?php post_class(); ?>>
   <header>
-    <h2 class="entry-title" data-postid="<?php get_the_ID() ?>">
-      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    <h2 class="entry-title" data-postid="<?php $pid = get_the_ID(); echo $pid; ?>">
+      <a href="<?php the_permalink(); ?>">ID: <?php $pid = get_the_ID(); echo $pid; ?>, <?php the_title(); ?></a>
     </h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
