7d93fad07fd8c2c36b5c045fcbaa09e060553949
Karuto <iamkaruto@gmail.com>
2016-01-23 23:51:59 -0800

    Added load ajax content with WP_Query

--- lib/ajax.php
+++ lib/ajax.php
@@ -7,5 +7,36 @@ namespace Roots\Sage\Ajax;
  */
 function my_enqueue_assets() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-    wp_enqueue_script( 'ajax-pagination',  get_stylesheet_directory_uri() . '/js/ajax-pagination.js', array( 'jquery' ), '1.0', true );
+    wp_enqueue_script( 'ajax-pagination',  get_stylesheet_directory_uri() . '../js/ajax-pagination.js', array( 'jquery' ), '1.0', true );
 }
+
+function my_load_ajax_content () {
+    $pid        = intval($_POST['post_id']);
+    $the_query  = new WP_Query(array('p' => $pid));
+
+    if ($the_query->have_posts()) {
+        while ( $the_query->have_posts() ) {
+            $the_query->the_post();
+
+            $data = '
+            <div class="post-container">
+                <div id="project-content">
+                    <h1 class="entry-title">'.get_the_title().'</h1>
+                    <div class="entry-content">'.get_the_content().'</div>
+                </div>
+            </div>  
+            ';
+
+        }
+    } 
+    else {
+        echo '<div id="postdata">'.__('Didnt find anything', THEME_NAME).'</div>';
+    }
+    wp_reset_postdata();
+
+
+    echo '<div id="postdata">'.$data.'</div>';
+}
+
+add_action ( 'wp_ajax_nopriv_load-content', 'my_load_ajax_content' );
+add_action ( 'wp_ajax_load-content', 'my_load_ajax_content' );
