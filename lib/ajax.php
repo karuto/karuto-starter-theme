11c6ebde33518c81a1ae5d251be8b18baef1b0d7
Karuto <iamkaruto@gmail.com>
2016-01-23 23:52:33 -0800

    Fixed indentation

--- lib/ajax.php
+++ lib/ajax.php
@@ -11,31 +11,30 @@ function my_enqueue_assets() {
 }
 
 function my_load_ajax_content () {
-    $pid        = intval($_POST['post_id']);
-    $the_query  = new WP_Query(array('p' => $pid));
+  $pid        = intval($_POST['post_id']);
+  $the_query  = new WP_Query(array('p' => $pid));
 
-    if ($the_query->have_posts()) {
-        while ( $the_query->have_posts() ) {
-            $the_query->the_post();
+  if ($the_query->have_posts()) {
+    while ( $the_query->have_posts() ) {
+      $the_query->the_post();
 
-            $data = '
-            <div class="post-container">
-                <div id="project-content">
-                    <h1 class="entry-title">'.get_the_title().'</h1>
-                    <div class="entry-content">'.get_the_content().'</div>
-                </div>
-            </div>  
-            ';
+      $data = '
+      <div class="post-container">
+        <div id="project-content">
+          <h1 class="entry-title">'.get_the_title().'</h1>
+          <div class="entry-content">'.get_the_content().'</div>
+        </div>
+      </div>  
+      ';
 
-        }
-    } 
-    else {
-        echo '<div id="postdata">'.__('Didnt find anything', THEME_NAME).'</div>';
     }
-    wp_reset_postdata();
+  } 
+  else {
+    echo '<div id="postdata">'.__('Didnt find anything', THEME_NAME).'</div>';
+  }
+  wp_reset_postdata();
 
-
-    echo '<div id="postdata">'.$data.'</div>';
+  echo '<div id="postdata">'.$data.'</div>';
 }
 
 add_action ( 'wp_ajax_nopriv_load-content', 'my_load_ajax_content' );
