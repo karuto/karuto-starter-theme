76a24d4b71a23d171071365f679bf1a159bed65d
Karuto <iamkaruto@gmail.com>
2016-01-26 08:56:21 -0800

    Added basic markup

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,13 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+    <a class="brand karu-logo" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    <nav class="karu-global-navi nav-primary">
+      <!-- Note: I will support WordPress menu in future.
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
       endif;
-      ?>
+      ?> -->
     </nav>
   </div>
 </header>
