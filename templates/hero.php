4cce71ebe32281617a91063a0c2a5ab238cebf35
Vincent Zhang <vzhang@opentable.com>
2016-02-13 20:52:20 -0800

    Fixed mobile responsive layout for single

--- templates/hero.php
+++ templates/hero.php
@@ -1,22 +1,25 @@
 <section class="hero">
   <div class="hero__wrapper container">
-    <h1 class="hero__text">
+    <div class="hero__wrapper__content">
 
     <?php if ( is_singular() ): global $post;
       if (has_post_thumbnail( $post->ID ) ) {
         $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
       }
     ?>
-      <?php single_post_title(); ?>
+      <h1 class="hero__text"><?php single_post_title(); ?></h1>
+
     <?php endif; ?>
 
     <?php if ( is_front_page() && is_home() ): ?>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>
-      Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
-      Ut enim ad minim veniam, quis nostrud exercitation.<br/>
+      <h1 class="hero__text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
+        Ut enim ad minim veniam, quis nostrud exercitation.<br/>
+      </h1>
     <?php endif; ?>
 
-    </h1>
+    </div>
   </div>
 
   <?php if ( is_singular() ): ?>
