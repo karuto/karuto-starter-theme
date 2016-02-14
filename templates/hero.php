bec8dc43707ff479da5acd9d2abe42821b3af1ce
Vincent Zhang <vzhang@opentable.com>
2016-02-13 21:39:19 -0800

    Fixed responsive

--- templates/hero.php
+++ templates/hero.php
@@ -1,18 +1,22 @@
 <section class="hero">
   <div class="hero__wrapper container">
-    <div class="hero__wrapper__content">
+    <div class="hero__content">
 
     <?php if ( is_singular() ): global $post;
       if (has_post_thumbnail( $post->ID ) ) {
         $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
       }
     ?>
-      <h1 class="hero__text"><?php single_post_title(); ?></h1>
-
+      <h1 class="hero__content__header"><?php single_post_title(); ?></h1>
+      <div class="hero__content__meta">
+        <h2 class="hero__content__meta__excerpt">
+          写到这里，我已渐渐忘记了自己初衷是想要纪念一座城市，还是纪念一个人。或许一切并没有那么奇怪。
+        </h2>
+      </div>
     <?php endif; ?>
 
     <?php if ( is_front_page() && is_home() ): ?>
-      <h1 class="hero__text">
+      <h1 class="hero__content__header">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>
         Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
         Ut enim ad minim veniam, quis nostrud exercitation.<br/>
