3b67d9b1ef7ec109dc22405c49a722260f5ea7ee
Vincent Zhang <vzhang@opentable.com>
2016-02-14 18:46:38 -0800

    Added flexbox mixin

--- templates/content.php
+++ templates/content.php
@@ -1,9 +1,9 @@
 <article <?php post_class('feed-item'); ?>>
-  <header class="feed-item__header clearfix">
+  <header class="feed-item__header">
     <h3 class="feed-item__title" data-postid="<?php $pid = get_the_ID(); echo $pid; ?>">
       <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
     </h3>
     <summary class="feed-item__excerpt"><?php the_excerpt(); ?></summary>
-    <time class="feed-item__time updated" datetime="<?= get_post_time('c', true); ?>"><?php the_time('M d, Y'); ?></time>
+    <time class="feed-item__time updated" datetime="<?= get_post_time('c', true); ?>">5 minute read - <?php the_time('M d, Y'); ?></time>
   </header>
 </article>
--- templates/feed.php
+++ templates/feed.php
@@ -1,15 +1,29 @@
-<section class="feed feed--featured">
+
 
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
     <?php _e('Sorry, no results were found.', 'sage'); ?>
   </div>
   <?php get_search_form(); ?>
+<?php else: ?>
+
+<div class="primary-feed-container">
+  <div class="flex-stretch col-lg-6">
+    <section class="feed feed--singlehero">
+      <article class="feed-item">
+      </article>
+    </section>
+  </div>
+
+  <div class="col-lg-6">
+    <section class="feed feed--featured">
+      <?php while (have_posts()) : the_post(); ?>
+        <?php get_template_part('templates/content', 
+        get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+      <?php endwhile; ?>
+    </section>
+  </div>
+</div><!-- .primary-feed-container -->
 <?php endif; ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 
-  get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
 
-</section>
