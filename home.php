7b00afc9778b25ca16f879d155919bba80a18017
Vincent Zhang <vzhang@opentable.com>
2016-02-15 23:51:36 -0800

    Imported modules on homepage

--- home.php
+++ home.php
@@ -3,3 +3,32 @@
 
 <!-- ### CONTENT FEED -->
 <?php get_template_part('templates/feed'); ?>
+
+<div class="modules-container row">
+  <div class="col-lg-6 col-md-12 col-sm-12">
+    <section class="module">
+      <div class="feed__header">
+        <div class="feed__header__text">过往文章</div>
+        <div class="feed__header__meta">Sorted by date</div>
+      </div>
+    </section>
+  </div>
+
+  <div class="col-lg-3 col-md-6 col-sm-6">
+    <section class="module">
+      <div class="feed__header">
+        <div class="feed__header__text">过往文章</div>
+        <div class="feed__header__meta">Sorted by date</div>
+      </div>
+    </section>
+  </div>
+
+  <div class="col-lg-3 col-md-6 col-sm-6">
+    <section class="module">
+      <div class="feed__header">
+        <div class="feed__header__text">过往文章</div>
+        <div class="feed__header__meta">Sorted by date</div>
+      </div>
+    </section>
+  </div>
+</div><!-- .row -->
