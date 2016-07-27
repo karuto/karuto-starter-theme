7eddf7765571a4e4737d75f5be6d76ded8a0da57
Vincent Zhang <vzhang@opentable.com>
2016-07-27 16:36:12 -0700

    Cleaned up header code

--- templates/head.php
+++ templates/head.php
@@ -3,5 +3,5 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
-  <link href="https://fonts.googleapis.com/css?family=Merriweather:400|Source+Sans+Pro:300,400,600" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Merriweather:400|Source+Sans+Pro:300,400,700" rel="stylesheet">
 </head>
--- templates/header.php
+++ templates/header.php
@@ -1,22 +1,19 @@
-<header class="wrap--header__banner">
-  <div class="row">
-    <div class="col-xs-12">
-      <a class="wrap--header__banner__logo" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-      <nav class="wrap--header__banner__navi nav-primary">
-        <div class="menu-all-pages-container">
-          <ul id="menu-all-pages" class="nav">
-            <li class="menu-item"><a href="#">About</a></li>
-            <li class="menu-item"><a href="#">Writings</a></li>
-            <li class="menu-item"><a href="#">Projects</a></li>
-          </ul>
-        </div>
-        <!-- Note: I will support WordPress menu in future.
-        <?php
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-        endif;
-        ?> -->
-      </nav>
+<header class="header">
+  <a class="header__logo" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+  <nav class="header__nav">
+    <div class="header__nav__group float-left">
+      <ul class="nav">
+        <li class="menu-item"><a href="#">About</a></li>
+        <li class="menu-item"><a href="#">Writings</a></li>
+        <li class="menu-item"><a href="#">Projects</a></li>
+      </ul>
     </div>
-  </div>
+    <div class="header__nav__group float-right">
+      <ul class="nav">
+        <li class="menu-item"><a href="#">Douban</a></li>
+        <li class="menu-item"><a href="#">Instagram</a></li>
+        <li class="menu-item"><a href="#">Newsletter</a></li>
+      </ul>
+    </div>
+  </nav>
 </header>
