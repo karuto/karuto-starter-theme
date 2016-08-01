160801d75c3896bc2ba107eb5844b49a604cf4b2
Vincent Zhang <vzhang@opentable.com>
2016-08-01 16:19:16 -0700

    Added archive template, fixed looping

--- templates/feeds.php
+++ templates/feeds.php
@@ -7,24 +7,16 @@
   <?php get_search_form(); ?>
 <?php else: /* If there exists posts */ ?>
 
-  <?php if( is_front_page() && is_home() ): /* HOMEPAGE FEED */ ?>
+  <div class="feeds__header">
+    <?= Titles\title(); ?> - All articles / Only the best
+  </div>
 
-    <div class="feeds__header">
-      All articles / Only the best
-    </div>
-
-    <div class="module feeds">
-      <?php global $post; $myposts = get_posts('numberposts=10');
-       foreach($myposts as $post) : ?>
-        <?php get_template_part('templates/feed', 
-        get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-      <?php endforeach; ?>
-    </div>
-
-  <?php else: /* NORMAL ARCHIVE FEED*/ ?>
-  <?php endif; ?>
-
-
+  <div class="module feeds">
+    <?php while ( have_posts() ) : the_post(); ?>
+      <?php get_template_part('templates/feed', 
+      get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+    <?php endwhile; ?>
+  </div>
 
 <?php endif; ?>
 
