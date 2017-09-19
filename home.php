2fde24bdd4478c6bd7cf4982628c2c2de8387b21
Vincent Zhang <vzhang@opentable.com>
2017-09-19 17:07:24 +0100

    Edited hero

--- home.php
+++ home.php
@@ -1,11 +1,11 @@
 <!-- ### CONTENT FEED -->
 <section class="hero hero--home">
   <h2 class="hero__title">
-    Title here
+    Hi there.
   </h2>
   <p class="hero__content">
-    Vincent Zhang is a frontend engineer working at <s>MIT</s> <s>his startup</s> <a href="#">OpenTable.</a><br/>
-    He writes about <a href="#">CSS</a> and what he's doing <a href="#">now</a>. You can also <a href="#">subscribe for updates.</a>
+    Vincent Zhang is a frontend engineer working at <s class="hero__s">MIT</s> <s class="hero__s">his startup</s> <a class="hero__link" href="https://github.com/karuto">OpenTable.</a><br/>
+    He writes about <a class="hero__link" href="#">CSS</a> and what he's doing <a class="hero__link" href="#">now</a>. You can also <a class="hero__link" href="#">subscribe for updates.</a>
   </p>
 </section>
 
