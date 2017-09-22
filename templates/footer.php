c7a37804a6559b09e3c995aeddd21ab4834bd6d6
Vincent Zhang <vzhang@opentable.com>
2017-09-22 12:26:41 +0100

    Fixed header, introduced Lato font for logo

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,6 @@
 <footer role="footer">
 <nav class="footer__nav">
+  <a href="">&copy; 2017</a>
   <a class="footer__link" href="#">Sitemap</a>
   <a class="footer__link" href="#">Contact</a>
   <a class="footer__link" href="/wp-admin">Login</a>
