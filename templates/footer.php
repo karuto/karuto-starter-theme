bcaadb8d570c8c772e5db6310a76755b30d1ccc5
Vincent Zhang <vzhang@opentable.com>
2017-09-22 13:04:23 +0100

    Fixed post navi, entry meta

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,12 @@
 <footer role="footer">
   <div class="footer__logo">
     <a class="footer__logo__link" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    &copy; 2017
+    <aside class="footer__logo__copyright">&copy; 2017</aside>
   </div>
   <nav class="footer__nav">
     <a class="footer__link" href="#">Sitemap</a>
     <a class="footer__link" href="#">Contact</a>
+    <!-- GitHub, LinkedIn, Email -->
     <a class="footer__link" href="/wp-admin">Login</a>
     <a class="footer__link" href="#">Search</a>
   </nav>
