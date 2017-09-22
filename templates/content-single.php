afef22989f6b1fbedb896ed371296c526f10c54e
Vincent Zhang <vzhang@opentable.com>
2017-09-22 13:41:41 +0100

    Clean up meta

--- templates/content-single.php
+++ templates/content-single.php
@@ -2,24 +2,13 @@
   <article <?php post_class(); ?>>
   <div class="entry">
 
-    <div class="hero">
+    <div class="hero hero--entry">
+      <aside class="entry__meta">
+        <?php the_category(' &bull; '); ?> &bull; Written On <?php the_time('M d, Y'); ?>
+      </aside>
       <h1 class="hero__title"><?php single_post_title(); ?></h1>
+      <aside class="entry__excerpt"><?php echo get_the_excerpt(); ?></aside>
     </div>
-
-    <aside class="entry__metas">
-      <dl class="entry__meta entry__meta--summary">
-        <dt class="entry__meta__title">Summary</dt>
-        <dd><?php echo get_the_excerpt(); ?></dd>
-      </dl>
-      <dl class="entry__meta">
-        <dt class="entry__meta__title">Written On</dt>
-        <dd><?php the_time('M d, Y'); ?></dd>
-      </dl>
-      <dl class="entry__meta">
-        <dt class="entry__meta__title">Category</dt>
-        <dd><?php the_category(' &bull; '); ?></dd>
-      </dl>
-    </aside>
 
     <div class="entry__content">
       <?php the_content(); ?>
