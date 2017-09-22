5d65256a5e5c34683d98d985b781ccf79e914155
Vincent Zhang <vzhang@opentable.com>
2017-09-22 12:38:50 +0100

    Fixed typography variables

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,12 @@
 <footer role="footer">
-<nav class="footer__nav">
-  <a href="">&copy; 2017</a>
-  <a class="footer__link" href="#">Sitemap</a>
-  <a class="footer__link" href="#">Contact</a>
-  <a class="footer__link" href="/wp-admin">Login</a>
-  <a class="footer__link" href="#">Search</a>
-</nav>
+  <div class="footer__logo">
+    <a class="footer__logo__link" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    &copy; 2017
+  </div>
+  <nav class="footer__nav">
+    <a class="footer__link" href="#">Sitemap</a>
+    <a class="footer__link" href="#">Contact</a>
+    <a class="footer__link" href="/wp-admin">Login</a>
+    <a class="footer__link" href="#">Search</a>
+  </nav>
 </footer>
