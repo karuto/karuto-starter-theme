6ef9530ea73458b9643440cae37b6f1244828901
Vincent Zhang <vzhang@opentable.com>
2017-09-23 12:28:39 +0100

    Added custom search form

--- index.php
+++ index.php
@@ -1,8 +1,2 @@
 <!-- ### CONTENT FEED -->
-<?php get_template_part('templates/feed'); ?>
-
-<?php the_posts_navigation(array(
-            'prev_text'          => __( 'Previous' ),
-            'next_text'          => __( 'Next' ),
-            'screen_reader_text' => __( 'Posts navigation' ),
-)); ?>
+<?php get_template_part('templates/feeds'); ?>
