aef5aff7aefe54b275aef75c4f87b5cafafa611c
Vincent Zhang <vzhang@opentable.com>
2017-09-23 12:09:21 +0100

    Componentized feeds for search and 404, feels awesome

---
+++ templates/feeds-header.php
@@ -0,0 +1,5 @@
+<?php use Roots\Sage\Titles; ?>
+
+<div class="feeds__header">
+  <?= Titles\title(); ?><!-- - All articles / Only the best -->
+</div>
--- templates/feeds.php
+++ templates/feeds.php
@@ -1,27 +1,5 @@
-<?php use Roots\Sage\Titles; ?>
-
 <?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
+  <?php get_template_part('templates/feeds-oops'); ?>
 <?php else: /* If there are posts */ ?>
-
-  <section class="feeds">
-    <div class="feeds__header">
-      <?= Titles\title(); ?><!-- - All articles / Only the best -->
-    </div>
-
-    <?php while ( have_posts() ) : the_post(); ?>
-      <?php get_template_part('templates/feed',
-      get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-    <?php endwhile; ?>
-
-    <?php the_posts_navigation(array(
-                'prev_text'          => __( 'Previous' ),
-                'next_text'          => __( 'Next' ),
-                'screen_reader_text' => __( 'Posts navigation' ),
-    )); ?>
-  </section>
-
+  <?php get_template_part('templates/feeds-core'); ?>
 <?php endif; ?>
