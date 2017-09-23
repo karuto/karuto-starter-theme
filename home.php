3058f94640d37b7e5dc238b969f694db926197c3
Vincent Zhang <vzhang@opentable.com>
2017-09-23 11:25:46 +0100

    Improved markup in entry, break out entry content

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
     Hi there.
   </h2>
   <p class="hero__content">
-    I'm Vincent Zhang, a frontend engineer working at <s class="hero__s" href="#">MIT</s> <s class="hero__s" href="#">his startup</s> <a class="hero__link" target="_blank" href="https://github.com/karuto">OpenTable.</a><br/>
+    I'm Vincent Zhang, a frontend engineer working at <s class="hero__s" href="#">MIT</s> <s class="hero__s" href="#">my startup</s> <a class="hero__link" target="_blank" href="https://github.com/karuto">OpenTable.</a><br/>
     I write about <a class="hero__link" href="/category/dev">CSS</a> and what I'm doing <a class="hero__link" href="/now">now</a>. You should subscribe for <a class="hero__link" href="#">updates.</a>
   </p>
 </section>
