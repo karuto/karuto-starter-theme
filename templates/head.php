a53bfaefa9d83dfb055c94c62df28be40bdcd83c
Vincent Zhang <vzhang@opentable.com>
2017-10-06 15:14:15 +0100

    Added meta tags, removed sidebar

--- templates/head.php
+++ templates/head.php
@@ -2,6 +2,9 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="web, design, frontend, front-end, user experience, html, css, html5, css3, javascript, reactjs, technology, essay">
+  <meta name="description" content="Vincent Zhang professional portfolio of modern front-end technologies. Articles on JavaScript, CSS, HTML and long-form essays.">
   <?php wp_head(); ?>
+  <link rel="copyright" href="//www.vincentzh.com/copyright">
   <link href="https://fonts.googleapis.com/css?family=Lato:700|Source+Code+Pro:400:700|Merriweather:400,700|Source+Sans+Pro:300,400,700" rel="stylesheet">
 </head>
