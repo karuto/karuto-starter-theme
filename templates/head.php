b43b65d95dff5aef170982783baabaf94aaa285c
Karuto <iamkaruto@gmail.com>
2017-10-21 15:49:16 +0100

    Adjusted typography in feed, removed font

--- templates/head.php
+++ templates/head.php
@@ -3,8 +3,8 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="keywords" content="web, design, frontend, front-end, user experience, html, css, html5, css3, javascript, reactjs, technology, essay">
-  <meta name="description" content="Vincent Zhang professional portfolio of modern front-end technologies. Articles on JavaScript, CSS, HTML and long-form essays.">
-  <?php wp_head(); ?>
+  <meta name="description" content="Vincent Zhang is an engineer specializing in modern front-end technologies. This website contains professional portfolio information, projects and articles on JavaScript, CSS, HTML and other long-form essays.">
   <link rel="copyright" href="//www.vincentzh.com/copyright">
   <link href="https://fonts.googleapis.com/css?family=Noto+Serif|Roboto:400,700|" rel="stylesheet">
+  <?php wp_head(); ?>
 </head>
