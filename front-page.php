1705f8ca82a3ab78cf4b80fd38be2a8468595c11
Karuto <iamkaruto@gmail.com>
2018-07-30 01:09:00 +0100

    Remove newline

--- front-page.php
+++ front-page.php
@@ -6,9 +6,7 @@
   <p class="hero__content">
     Frontend engineer working at <s class="hero__s" href="#">MIT</s> <s class="hero__s" href="#">my startup</s> <a class="hero__link" target="_blank" href="https://github.com/karuto">OpenTable</a> in San Francisco / London.
     <br/>
-    I read  <a class="hero__link" href="/category/books">books</a>, write about <a class="hero__link" href="/category/dev">software</a> and what I'm doing <a class="hero__link" href="/now">now</a>.
-    <br/>
-    You should <a class="hero__link" target="_blank" href="//eepurl.com/c8ysuj">subscribe for updates.</a>
+    I read  <a class="hero__link" href="/category/books">books</a>, write about <a class="hero__link" href="/category/dev">software</a> and what I'm doing <a class="hero__link" href="/now">now</a>. You should <a class="hero__link" target="_blank" href="//eepurl.com/c8ysuj">subscribe for updates.</a>
   </p>
 </section>
 
