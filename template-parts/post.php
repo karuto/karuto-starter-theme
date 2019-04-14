93d61a5de0c66c9bb072938adf597aded31d7749
Vincent Zhang <iamkaruto@gmail.com>
2019-04-14 16:29:34 -0700

    Page and post templates using feed

--- template-parts/post.php
+++ template-parts/post.php
@@ -10,30 +10,4 @@
  */
 ?>
 
-<?php
-
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-
-	get_template_part( 'template-parts/content/content', 'single' );
-
-	// Previous/next post navigation.
-	the_post_navigation(
-		array(
-			'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'twentynineteen' ) . '</span> ' .
-				'<span class="screen-reader-text">' . __( 'Next post:', 'twentynineteen' ) . '</span> <br/>' .
-				'<span class="post-title">%title</span>',
-			'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'twentynineteen' ) . '</span> ' .
-				'<span class="screen-reader-text">' . __( 'Previous post:', 'twentynineteen' ) . '</span> <br/>' .
-				'<span class="post-title">%title</span>',
-		)
-	);
-
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template( '/template-parts/comments.php' );
-	}
-
-endwhile; // End of the loop.
-?>
+<?php get_template_part( 'template-parts/feed/feeds' ); ?>
