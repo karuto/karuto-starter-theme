aed462afd00ecd0757866416e919c7257b367248
Vincent Zhang <iamkaruto@gmail.com>
2019-04-14 23:24:16 -0700

    Removed thumbnail related functions; fix pagination

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -7,73 +7,6 @@
  * @since 1.0.0
  */
 
- if ( ! function_exists( 'twentynineteen_post_thumbnail' ) ) :
-	/**
-	 * Displays an optional post thumbnail.
-	 *
-	 * Wraps the post thumbnail in an anchor element on index views, or a div
-	 * element when on single views.
-	 */
-	function twentynineteen_post_thumbnail() {
-		if ( ! twentynineteen_can_show_post_thumbnail() ) {
-			return;
-		}
-
-		if ( is_singular() ) :
-			?>
-
-			<figure class="post-thumbnail">
-				<?php the_post_thumbnail(); ?>
-			</figure><!-- .post-thumbnail -->
-
-			<?php
-		else :
-			?>
-
-		<figure class="post-thumbnail">
-			<a class="post-thumbnail-inner" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-				<?php the_post_thumbnail( 'post-thumbnail' ); ?>
-			</a>
-		</figure>
-
-			<?php
-		endif; // End is_singular().
-	}
-endif;
-
-if ( ! function_exists( 'twentynineteen_comment_avatar' ) ) :
-	/**
-	 * Returns the HTML markup to generate a user avatar.
-	 */
-	function twentynineteen_get_user_avatar_markup( $id_or_email = null ) {
-
-		if ( ! isset( $id_or_email ) ) {
-			$id_or_email = get_current_user_id();
-		}
-
-		return sprintf( '<div class="comment-user-avatar comment-author vcard">%s</div>', get_avatar( $id_or_email, twentynineteen_get_avatar_size() ) );
-	}
-endif;
-
-if ( ! function_exists( 'twentynineteen_discussion_avatars_list' ) ) :
-	/**
-	 * Displays a list of avatars involved in a discussion for a given post.
-	 */
-	function twentynineteen_discussion_avatars_list( $comment_authors ) {
-		if ( empty( $comment_authors ) ) {
-			return;
-		}
-		echo '<ol class="discussion-avatar-list">', "\n";
-		foreach ( $comment_authors as $id_or_email ) {
-			printf(
-				"<li>%s</li>\n",
-				twentynineteen_get_user_avatar_markup( $id_or_email )
-			);
-		}
-		echo '</ol><!-- .discussion-avatar-list -->', "\n";
-	}
-endif;
-
 if ( ! function_exists( 'twentynineteen_comment_form' ) ) :
 	/**
 	 * Documentation for function.
@@ -102,11 +35,11 @@ if ( ! function_exists( 'twentynineteen_the_posts_navigation' ) ) :
 				'prev_text' => sprintf(
 					'%s <span class="nav-prev-text">%s</span>',
 					twentynineteen_get_icon_svg( 'chevron_left', 22 ),
-					__( 'Newer posts', 'twentynineteen' )
+					''
 				),
 				'next_text' => sprintf(
 					'<span class="nav-next-text">%s</span> %s',
-					__( 'Older posts', 'twentynineteen' ),
+					'',
 					twentynineteen_get_icon_svg( 'chevron_right', 22 )
 				),
 			)
--- templates/partials/header.php
+++ templates/partials/header.php
@@ -12,7 +12,7 @@
 
 <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentynineteen' ); ?></a>
 
-<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
+<header id="masthead" class="site-header">
 
 	<div class="site-branding-container">
 		<?php get_template_part( 'templates/partials/header-site-branding' ); ?>
