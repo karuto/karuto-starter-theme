ae91e78b3fc2f0e92295cd29cbd438e4ef97fff7
Vincent Zhang <iamkaruto@gmail.com>
2019-04-16 12:00:13 -0700

    Moved out opinionated parts: page-home, tracking scripts, created end-of-body component

--- templates/partials/html-closing-tags.php
+++ templates/partials/html-closing-tags.php
@@ -23,10 +23,6 @@
 			<?php get_template_part( 'templates/partials/footer' ); ?>
 		</div><!-- #page -->
 
-		<!-- TODO: this section below is specific to the site. Make it adaptable later. -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-41636874-3"></script>
-    <script>
-      window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-41636874-3');
-    </script>
+		<?php get_template_part( 'templates/partials/html-end-of-body' ); ?>
 	</body>
 </html>
