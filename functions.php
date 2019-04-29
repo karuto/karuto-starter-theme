721fc396e9b08e918d8c180134717a58a6139892
Vincent Zhang <iamkaruto@gmail.com>
2019-04-28 18:21:32 -0700

    Remove menu-touch-keyboard-navigation

--- functions.php
+++ functions.php
@@ -218,10 +218,6 @@ add_action( 'after_setup_theme', 'twentynineteen_content_width', 0 );
 function twentynineteen_scripts() {
 	wp_enqueue_style( 'karuto-starter-theme-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
 
-	if ( has_nav_menu( 'header' ) ) {
-		wp_enqueue_script( 'twentynineteen-touch-navigation', get_theme_file_uri( '/js/menu-touch-keyboard-navigation.js' ), array(), '1.1', true );
-	}
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
